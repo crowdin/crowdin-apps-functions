70716ded7dc9f4c59897b674341646f61a38ae45
Maurizio Pozzobon <maurizio@oiritaly.it>
2022-10-23 19:01:50 +0200

    test refactoring

--- tests/crowdin.spec.ts
+++ tests/crowdin.spec.ts
@@ -1,47 +1,15 @@
 import { updateOrCreateFile } from '../src';
 import Crowdin, { ResponseObject, SourceFilesModel, UploadStorageModel } from '@crowdin/crowdin-api-client';
 import { createMock } from 'ts-auto-mock';
+
 describe('UpdateOrCreateFile function', () => {
-    it('should create file when source file is undefined', async () => {
-        let spyProjectId: number;
-        let spyRequest: SourceFilesModel.CreateFileRequest;
-        const client = createMock<Crowdin>({
-            uploadStorageApi: {
-                addStorage: (): Promise<ResponseObject<UploadStorageModel.Storage>> => {
-                    return createMock<Promise<ResponseObject<UploadStorageModel.Storage>>>();
-                },
-            },
-            sourceFilesApi: {
-                createFile: (
-                    projectId: number,
-                    request: SourceFilesModel.CreateFileRequest,
-                ): Promise<ResponseObject<SourceFilesModel.File>> => {
-                    spyProjectId = projectId;
-                    spyRequest = request;
-                    return createMock<Promise<ResponseObject<SourceFilesModel.File>>>();
-                },
-            },
-        });
-        await updateOrCreateFile(client, 1, 'name', 'title', 'auto', 2, null, undefined).then(result => {
-            expect(result).toBe(0);
-            expect(spyProjectId).toBe(1);
-            expect(spyRequest).toStrictEqual({
-                storageId: 0,
-                name: 'name',
-                title: 'title',
-                type: 'auto',
-                directoryId: 2,
-            });
-        });
-    });
-    it('should update file when source file is defined', async () => {
-        let spyProjectId: number;
-        let spyFileId: number;
-        let spyRequest: SourceFilesModel.ReplaceFileFromStorageRequest | SourceFilesModel.RestoreFile;
-        const file = createMock<SourceFilesModel.File>({
-            id: 3,
-        });
-        const client = createMock<Crowdin>({
+    let client: Crowdin;
+    let spyProjectId: number;
+    let spyFileId: number;
+    let spyRequest: SourceFilesModel.ReplaceFileFromStorageRequest | SourceFilesModel.RestoreFile;
+
+    beforeEach(() => {
+        client = createMock<Crowdin>({
             uploadStorageApi: {
                 addStorage: (): Promise<ResponseObject<UploadStorageModel.Storage>> => {
                     return createMock<Promise<ResponseObject<UploadStorageModel.Storage>>>();
@@ -58,8 +26,37 @@ describe('UpdateOrCreateFile function', () => {
                     spyFileId = fileId;
                     return createMock<Promise<ResponseObject<SourceFilesModel.File>>>();
                 },
+                createFile: (
+                    projectId: number,
+                    request: SourceFilesModel.CreateFileRequest,
+                ): Promise<ResponseObject<SourceFilesModel.File>> => {
+                    spyProjectId = projectId;
+                    spyRequest = request;
+                    return createMock<Promise<ResponseObject<SourceFilesModel.File>>>();
+                },
             },
         });
+    });
+
+    it('should create file when source file is undefined', async () => {
+        await updateOrCreateFile(client, 1, 'name', 'title', 'auto', 2, null, undefined).then(result => {
+            expect(result).toBe(0);
+            expect(spyProjectId).toBe(1);
+            expect(spyRequest).toStrictEqual({
+                storageId: 0,
+                name: 'name',
+                title: 'title',
+                type: 'auto',
+                directoryId: 2,
+            });
+        });
+    });
+
+    it('should update file when source file is defined', async () => {
+        const file = createMock<SourceFilesModel.File>({
+            id: 3,
+        });
+
         await updateOrCreateFile(client, 1, 'name', 'title', 'auto', 2, null, file).then(result => {
             expect(result).toBe(3);
             expect(spyProjectId).toBe(1);
