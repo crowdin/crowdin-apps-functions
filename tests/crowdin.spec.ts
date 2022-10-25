8d413a69223724bcf4847fe65dec79d62d6f2465
Maurizio Pozzobon <maurizio@oiritaly.it>
2022-10-25 18:46:50 +0200

    Cover getFolder in the crowdin.ts class #21

--- tests/crowdin.spec.ts
+++ tests/crowdin.spec.ts
@@ -1,5 +1,11 @@
-import { updateOrCreateFile } from '../src';
-import Crowdin, { ResponseObject, SourceFilesModel, UploadStorageModel } from '@crowdin/crowdin-api-client';
+import { updateOrCreateFile, getFolder } from '../src';
+import Crowdin, {
+    ResponseList,
+    ResponseObject,
+    SourceFiles,
+    SourceFilesModel,
+    UploadStorageModel,
+} from '@crowdin/crowdin-api-client';
 import { createMock } from 'ts-auto-mock';
 
 describe('UpdateOrCreateFile function', () => {
@@ -64,6 +70,72 @@ describe('UpdateOrCreateFile function', () => {
             expect(spyRequest).toStrictEqual({
                 storageId: 0,
             });
+        });
+    });
+});
+
+describe('getFolder function', () => {
+    let client: Crowdin;
+    let spyMaxLimit: number | undefined;
+    let spyProjectId: number;
+    let spyOptions: SourceFilesModel.ListProjectFilesOptions | undefined;
+    let file: SourceFilesModel.File;
+    const parentFolder = createMock<SourceFilesModel.Directory>({ name: 'parentFolder', id: 3 });
+    beforeEach(() => {
+        file = createMock<SourceFilesModel.File>();
+        client = createMock<Crowdin>({
+            sourceFilesApi: {
+                withFetchAll: (maxLimit?: number): SourceFiles => {
+                    spyMaxLimit = maxLimit;
+                    const mock = createMock<SourceFiles>();
+                    mock.listProjectFiles = ((
+                        projectId: number,
+                        options?: SourceFilesModel.ListProjectFilesOptions,
+                    ): Promise<ResponseList<SourceFilesModel.File>> => {
+                        spyProjectId = projectId;
+                        spyOptions = options;
+                        return new Promise<ResponseList<SourceFilesModel.File>>(resolve => {
+                            resolve(
+                                createMock<ResponseList<SourceFilesModel.File>>({
+                                    data: [
+                                        createMock<ResponseObject<SourceFilesModel.File>>({
+                                            data: file,
+                                        }),
+                                    ],
+                                }),
+                            );
+                        });
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    }) as any;
+                    return mock;
+                },
+            },
+        });
+    });
+
+    it('retrives folder and files in it', async () => {
+        const folder = createMock<SourceFilesModel.Directory>({ name: 'directoryName', id: 2 });
+        const directories = [folder];
+        await getFolder(directories, client, 1, folder.name).then(result => {
+            expect(result).toStrictEqual({ files: [file], folder: folder });
+            expect(spyMaxLimit).toBe(undefined);
+            expect(spyProjectId).toBe(1);
+            expect(spyOptions).toStrictEqual({ directoryId: 2 });
+        });
+    });
+
+    it('filters by parentFolder', async () => {
+        const folder = createMock<SourceFilesModel.Directory>({
+            name: 'directoryName',
+            id: 2,
+            directoryId: parentFolder.id,
+        });
+        const directories = [folder];
+        await getFolder(directories, client, 1, folder.name, parentFolder).then(result => {
+            expect(result).toStrictEqual({ files: [file], folder: folder });
+            expect(spyMaxLimit).toBe(undefined);
+            expect(spyProjectId).toBe(1);
+            expect(spyOptions).toStrictEqual({ directoryId: 2 });
         });
     });
 });
